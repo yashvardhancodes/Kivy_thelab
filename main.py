6fa36470634b87af2addb6c6c60739ee6fc3018c
yash <vardhancodeshub@gmail.com>
2021-06-08 15:09:20 +0530

    Stack Layout Complete

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from kivy.app import App
+from kivy.metrics import dp
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "lr-tb"  # l=left, r=right, t=top, b=bottom
+        self.padding = (dp(20), dp(20), dp(20), dp(20))
+        self.spacing = (dp(20), dp(20))
+        for i in range(0, 10):
+            b = Button(text=str(i + 1), size_hint=(None, None),
+                       size=(dp(100), dp(100)))
+            self.add_widget(b)
+
+
+# class GridLayoutExample(GridLayout):
+#     pass
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
 
 
 class BoxLayoutExample(BoxLayout):
